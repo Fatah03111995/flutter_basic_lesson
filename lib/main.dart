ee14fbcd812c63ca01bb103fad43bc02bf6a492b
Abdul Fatah <fatah03111995@gmail.com>
2024-01-22 16:15:55 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
+  //class yang digunakan untuk menampilkan tampilan,
+  //di isi dengan parameter type class widget
+}
+
+//tidak perlu menurunkan secara langsung dari class widget
+//sudah dibuatkan turunan kelas abstraknya
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('test');
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
