a86dc3fc28cfb2ddf300c3a3cde04361f0c3c58e
Abdul Fatah <fatah03111995@gmail.com>
2024-02-01 09:12:17 +0700

    feat : les5_theme init commit

--- lib/les3_pages/fp_ul1.dart
+++ lib/les3_pages/fp_ul1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic_lesson/pages_les3/first_page.dart';
-import 'package:flutter_basic_lesson/pages_les3/second_page.dart';
+import 'package:flutter_basic_lesson/les3_pages/first_page.dart';
+import 'package:flutter_basic_lesson/les3_pages/second_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
